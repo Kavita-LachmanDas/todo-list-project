68a5624d6d5bd4f390986cf3bd622f73cfa0a766
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-05-05 17:19:26 +0500

    Add files via upload

---
+++ todo.ts
@@ -0,0 +1,86 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer"
+import { todo } from "node:test";
+
+let todos = [];
+let condition = true;
+while(condition){
+let ques = await inquirer.prompt(
+    [
+        {
+            name : "ist",
+            type : "list",
+            message : "Select Your Operation ",
+            choices : ["ADD","UPDATE","VEIW","DELETE","EXIT"]
+        }
+    ]
+)
+//add element
+if(ques.ist == "ADD"){
+    let quest = await inquirer.prompt(
+        [
+            {
+                name : "sec",
+                type : "input",
+                message : "Add Items In The List: ",
+            }
+        ]
+    )
+    todos.push(quest.sec)
+    console.log(todos)
+}
+//update
+if(ques.ist == "UPDATE"){
+    let updateee:any = await inquirer.prompt(
+        [
+            {
+                name : "update",
+                type : "list",
+                message : "update items from the list: ",
+                choices : todos.map((item)=>item)
+            },
+            {
+                name : "up",
+                type : "input",
+                message : "Add Items In The List: ",
+            }
+        ]
+    )
+    let newtodo:any = todos.filter((val)=>val !== updateee.update)
+    todos = [...newtodo,updateee.up]
+    console.log(todos)
+}
+
+
+//veiw
+if(ques.ist == "VEIW"){
+    console.log("******TODO-LIST ITEMS******")
+    console.log(todos)
+}
+
+//delete
+if(ques.ist == "DELETE"){
+    
+    
+        let del = await inquirer.prompt([
+            {
+                name: "delete",
+                type: "list",
+                message: "select item to delete",
+                choices: todos.map(item => item)
+            },
+        ]);
+        let newtodo:any = todos.filter(val => val !== del.delete);
+        todos = [...newtodo];
+        console.log(todos);
+    }
+
+//exit
+
+if(ques.ist == "EXIT"){
+console.log("              \n**************'EXIT'*****************\n        ")
+condition = false
+}
+}
